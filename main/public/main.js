242bd5de999069b43e2655dcd0a055f700d701a2
Agent421 <eakoledoye@gmail.com>
2023-10-22 17:37:14 +0100

    backups + just need to add slideshow

---
+++ main/public/main.js
@@ -0,0 +1,13 @@
+// slideshow -> https://www.w3schools.com/w3css/w3css_slideshow.asp
+
+function slideshow() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(slideshow, 2000); // Change image every 2 seconds
+}
