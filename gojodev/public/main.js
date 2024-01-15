ba241edc1983908d7dbe147f2c6c2186626f8941
Agent421 <eakoledoye@gmail.com>
2024-01-15 23:30:21 +0000

    backups

--- gojodev/public/main.js
+++ gojodev/public/main.js
@@ -1,19 +1,19 @@
+function textSwitch() {
+    let phrase = ["a programmer", "a gamer", "GojoDev"];
+    let text = document.getElementById("text-switch");
+    let currentIndex = 0;
 
-/**
- * slideshow -> https://www.w3schools.com/w3css/w3css_slideshow.asp
- * https://github.com/Phineas/lanyard
- * https://github.com/xaronnn/js-lanyard/
- * https://htmx.org/
- */
-
-function slideshow() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    myIndex++;
-    if (myIndex > x.length) { myIndex = 1 }
-    x[myIndex - 1].style.display = "block";
-    setTimeout(slideshow, 2000); // Change image every 2 seconds
+    const intervalId = setInterval(() => {
+        let newText = phrase[currentIndex];
+        if (currentIndex < phrase.length) {
+            console.log(newText);
+            text.innerHTML = newText
+            text.classList.toggle("fadeIn");
+            currentIndex++;
+        } else {
+            clearInterval(intervalId);
+        }
+    }, 1000);
 }
+
+textSwitch();
