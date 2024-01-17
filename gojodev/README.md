8851034b7996f60cf07816e97e32686467c83570
Agent421 <eakoledoye@gmail.com>
2024-01-17 22:55:07 +0000

    removed some images cause of icons and added a favicon, added a common soon section, added extra animations

---
+++ gojodev/README.md
@@ -0,0 +1,20 @@
+
+```
+gojodev
+├─ .firebaserc
+├─ .gitignore
+├─ firebase.json
+├─ package-lock.json
+├─ package.json
+└─ public
+   ├─ 404.html
+   ├─ images
+   │  ├─ caopoints.webp
+   │  ├─ emmanuel.webp
+   │  ├─ favicon.ico
+   │  └─ gojodev.webp
+   ├─ index.html
+   ├─ main.js
+   └─ style.css
+
+```
--- gojodev/public/main.js
+++ gojodev/public/main.js
@@ -26,18 +26,58 @@ textSwitch();
 
 function iconAni() {
     let icons = document.querySelectorAll("i");
-    let index = 0;
-    let Iconinterval = setInterval(() => {
-        let currentIcon = icons[index];
-        if (!currentIcon) {
-            clearInterval(Iconinterval);
-            return;
-        }
+    let footer = document.getElementById("footer");
+    let footRect = footer.getBoundingClientRect();
 
-        currentIcon.classList.add("fadeIn");
-        index++;
+    if (footRect.top < -100) {
+        let index = 0;
+        let Iconinterval = setInterval(() => {
+            let currentIcon = icons[index];
+            if (!currentIcon) {
+                clearInterval(Iconinterval);
+                return;
+            }
 
-    }, 100)
+            if (index < 3) {
+                currentIcon.classList.add("pulseAnimation");
+                currentIcon.style.animationDelay = `${index * 0.5}s`;
+                currentIcon.classList.add("fadeIn");
+            }
+
+            else {
+                currentIcon.classList.add("fadeIn");
+                currentIcon.classList.add("jumpAnimation");
+                currentIcon.style.animationDelay = `${index * 0.3}s`;
+            }
+
+            index++;
+
+        }, 100)
+    }
 }
 
-iconAni()
+window.addEventListener("scroll", iconAni)
+
+function gojodev() {
+    let emmanuel = document.getElementById("emmanuel");
+    let index = 0;
+    setInterval(() => {
+
+        emmanuel.classList.remove("fadeIn");
+        emmanuel.offsetWidth;
+        emmanuel.classList.add("fadeIn");
+
+        if (index == 0) {
+            emmanuel.style = "border-radius: 50%";
+            emmanuel.src = "images/gojodev.webp";
+            index = 1;
+        }
+        else {
+            emmanuel.style = "border-radius: 20px";
+            emmanuel.src = "images/emmanuel.webp";
+            index = 0;
+        }
+    }, 3500)
+}
+
+gojodev()
