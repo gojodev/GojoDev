e08845abb6a2e8e495d19f03738d9048a8f1702f
gojodev <gojodev@users.noreply.github.com>
2025-04-13 00:53:40 +0100

    made the entire project component clickable

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -36,44 +36,46 @@ const Project = ({
   }, []);
 
   return (
-    <div className="center generalBlackBG box-shadow w-[95%] m-auto rb20px">
-      <div className="container-v w-[250px] h-[250px]">
-        <div className="center">
-          <div className="m-auto">
-            <Image src={img} dim="150px" />
-          </div>
-        </div>
-        <div className="center generalText white bg-white p-2 rb20px mt-3 box-shadow">
-          <div className="text-black m-auto">{name}</div>
-        </div>
-      </div>
-
-      <div className="container-v white rb20px m-auto">
-        <div className="w-[100%]">
-          <div className="generalText text-center">{desc}</div>
-          <div className="container-h center">
-            <div className="w-[50%]">
-              <a href={github} target="_blank">
-                <FontAwesomeIcon
-                  icon={["fab", "github"]}
-                  size="2x"
-                  className={`${githubStyle} pulseAnimation`}
-                />
-              </a>
-
-              <a href={webLink} target="_blank">
-                <FontAwesomeIcon
-                  icon={["fas", "globe"]}
-                  size="2x"
-                  className={`${webLinkStyle} pulseAnimation`}
-                />
-              </a>
+    <a href={webLink || github} target="_blank">
+      <div className="center generalBlackBG box-shadow w-[95%] m-auto rb20px">
+        <div className="container-v w-[250px] h-[250px]">
+          <div className="center">
+            <div className="m-auto">
+              <Image src={img} dim="150px" />
             </div>
-            <div className="generalText blueText w-[50%]">{techStack}</div>
+          </div>
+          <div className="center generalText white bg-white p-2 rb20px mt-3 box-shadow">
+            <div className="text-black m-auto">{name}</div>
+          </div>
+        </div>
+
+        <div className="container-v white rb20px m-auto">
+          <div className="w-[100%]">
+            <div className="generalText text-center">{desc}</div>
+            <div className="container-h center">
+              <div className="w-[50%]">
+                <a href={github} target="_blank">
+                  <FontAwesomeIcon
+                    icon={["fab", "github"]}
+                    size="2x"
+                    className={`${githubStyle} pulseAnimation`}
+                  />
+                </a>
+
+                <a href={webLink} target="_blank">
+                  <FontAwesomeIcon
+                    icon={["fas", "globe"]}
+                    size="2x"
+                    className={`${webLinkStyle} pulseAnimation`}
+                  />
+                </a>
+              </div>
+              <div className="generalText blueText w-[50%]">{techStack}</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
