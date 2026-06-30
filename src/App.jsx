901e186fe5aed80916f7d19fb3d2169176e4c8f5
gojodev <gojodev@users.noreply.github.com>
2026-06-30 19:02:22 +0100

    updated link

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
             img="./images/caopoints.webp"
             desc="Shows the easiest way grades that are needed to get into College / University"
             github="https://github.com/gojodev/caopoints"
-            webLink="https://caopoints.web.app"
+            webLink="https://caopoints-info.web.app"
             name="Caopoints"
             techStack="HTML, CSS, JS, NodeJS, Firebase"
           />
